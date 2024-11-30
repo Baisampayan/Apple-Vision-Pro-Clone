2b76b9e303d3757f1e278f6c2815606761082f95
Baisampayan Das <baisampayandas@gmail.com>
2024-12-01 04:02:44 +0530

    Section-29 with bug

--- script.js
+++ script.js
@@ -559,3 +559,118 @@ function canvas2() {
     });
 }
 canvas2();
+
+
+// The Third Canvas
+function canvas3() {
+    const canvas = document.querySelector("#section-29>canvas");
+    const context = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+
+    window.addEventListener("resize", function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    render();
+    });
+
+    function files(index) {
+    var data = `
+        // paste all images here!!
+        ./assets/images/Vision00001.png
+        ./assets/images/Vision00002.png
+        ./assets/images/Vision00003.png
+        ./assets/images/Vision00004.png
+        ./assets/images/Vision00005.png
+        ./assets/images/Vision00006.png
+        ./assets/images/Vision00007.png
+        ./assets/images/Vision00008.png
+        ./assets/images/Vision00009.png
+        ./assets/images/Vision00010.png
+        ./assets/images/Vision00011.png
+        ./assets/images/Vision00012.png
+        ./assets/images/Vision00013.png
+        ./assets/images/Vision00014.png
+        ./assets/images/Vision00015.png
+        ./assets/images/Vision00016.png
+        ./assets/images/Vision00017.png
+        ./assets/images/Vision00018.png
+        ./assets/images/Vision00019.png
+        ./assets/images/Vision00020.png
+        ./assets/images/Vision00021.png
+        ./assets/images/Vision00022.png
+        ./assets/images/Vision00023.png
+        ./assets/images/Vision00024.png
+        ./assets/images/Vision00025.png
+    `;
+    return data.split("\n")[index];
+    }
+
+    const frameCount = 26;
+
+    const images = [];
+    const imageSeq = {
+    frame: 1,
+    };
+
+    for (let i = 0; i < frameCount; i++) {
+    const img = new Image();
+    img.src = files(i);
+    images.push(img);
+    }
+
+    gsap.to(imageSeq, {
+    frame: frameCount - 1,
+    snap: "frame",
+    ease: `none`,
+    scrollTrigger: {
+        scrub: 0.15,
+        trigger: `#section-29`,
+        //   set start end according to preference
+        start: `top top`,
+        end: `80% top`,
+        scroller: `#main`,
+    },
+    onUpdate: render,
+    });
+
+    images[1].onload = render;
+
+    function render() {
+    scaleImage(images[imageSeq.frame], context);
+    }
+
+    function scaleImage(img, ctx) {
+    var canvas = ctx.canvas;
+    var hRatio = canvas.width / img.width;
+    var vRatio = canvas.height / img.height;
+    var ratio = Math.max(hRatio, vRatio);
+    var centerShift_x = (canvas.width - img.width * ratio) / 2;
+    var centerShift_y = (canvas.height - img.height * ratio) / 2;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(
+        img,
+        0,
+        0,
+        img.width,
+        img.height,
+        centerShift_x,
+        centerShift_y,
+        img.width * ratio,
+        img.height * ratio
+    );
+    }
+    ScrollTrigger.create({
+
+    trigger: "#section-29",
+    pin: true,
+    // markers:true,
+    scroller: `#main`,
+    //   set start end according to preference
+    start: `top top`,
+    end: `80% top`,
+    });
+}
+canvas3();
